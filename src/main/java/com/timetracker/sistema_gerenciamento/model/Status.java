ad80637799e0281a0213d0045f465b618b95b884
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-12 15:20:26 -0300

    feat: adicionar enum para Status de projeto

---
+++ src/main/java/com/timetracker/sistema_gerenciamento/model/Status.java
@@ -0,0 +1,9 @@
+package com.timetracker.sistema_gerenciamento.model;
+
+public enum Status {
+    PLANEJADO,  // Adicionando o valor "PLANEJADO"
+    EM_ANDAMENTO,
+    INICIADO,
+    PAUSADO,
+    CONCLUÍDO
+}
