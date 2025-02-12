66cbc20e1716144b2a5b7adc5550ba19b78b0482
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-12 15:20:38 -0300

    feat: criar entidade Projeto com atributos e relacionamentos

---
+++ src/main/java/com/timetracker/sistema_gerenciamento/model/Projeto.java
@@ -0,0 +1,97 @@
+package com.timetracker.sistema_gerenciamento.model;
+
+import jakarta.persistence.*;import java.util.Date;
+
+@Entity
+@Table(name = "projetos")  // Este é o nome da tabela no banco de dados
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+
+    @Temporal(TemporalType.DATE)  // Especificando que a data deve ser tratada como Date
+    private Date dataInicio;
+
+    @Temporal(TemporalType.DATE)  // Especificando que a data deve ser tratada como Date
+    private Date dataFim;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Prioridade prioridade;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario_responsavel")
+    private Usuario usuarioResponsavel;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Usuario getUsuarioResponsavel() {
+        return usuarioResponsavel;
+    }
+
+    public void setUsuarioResponsavel(Usuario usuarioResponsavel) {
+        this.usuarioResponsavel = usuarioResponsavel;
+    }
+}
