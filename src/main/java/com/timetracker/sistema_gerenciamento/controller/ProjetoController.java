a57d13387421f35ad3a392b7855415d7598f8623
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-12 23:22:51 -0300

    feat: adicionado o repositório de usuários para associar o projeto ao usuário responsável

--- src/main/java/com/timetracker/sistema_gerenciamento/controller/ProjetoController.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/controller/ProjetoController.java
@@ -1,9 +1,14 @@
 package com.timetracker.sistema_gerenciamento.controller;
+
+import com.timetracker.sistema_gerenciamento.model.Usuario;
+import com.timetracker.sistema_gerenciamento.repository.UsuarioRepository; // Importe o repositório de usuários
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.timetracker.sistema_gerenciamento.service.ProjetoService;
 import com.timetracker.sistema_gerenciamento.model.Projeto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/projetos")
@@ -12,8 +17,26 @@ public class ProjetoController {
     @Autowired
     private ProjetoService projetoService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;  // Injetando o UsuarioRepository
+
     @PostMapping
     public Projeto createProjeto(@RequestBody Projeto projeto) {
+        Long usuarioId = projeto.getUsuarioResponsavel().getId();  // Extrai o id do usuário responsável
+
+        // Verifique se o usuário existe
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        // Associa o usuário ao projeto
+        projeto.setUsuarioResponsavel(usuario);
+
+        // Salva o projeto associado ao usuário
         return projetoService.save(projeto);
+    }
+
+    @GetMapping("/usuario/{idUsuario}")
+    public List<Projeto> getProjetosPorUsuario(@PathVariable Long idUsuario) {
+        return projetoService.findProjetosByUsuario(idUsuario);
     }
 }
