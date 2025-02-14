fff9bec9a867b3abbe416aa0d264c495afc067e6
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-14 13:01:19 -0300

    feat: cria o repositorio de clientes

---
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.timetracker.sistema_gerenciamento.repository;
+
+import com.timetracker.sistema_gerenciamento.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Cliente findByEmailCliente(String email);
+}
