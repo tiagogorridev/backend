984485db484ada015f30cee62fb36f4363035ce1
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-14 14:17:38 -0300

    fix: corrige importação e mantém a consulta para projetos com clientes

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/ProjetoRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/ProjetoRepository.java
@@ -1,8 +1,8 @@
 package com.timetracker.sistema_gerenciamento.repository;
 
-import com.timetracker.sistema_gerenciamento.model.Projeto;  // Adicione esta linha
-
+import com.timetracker.sistema_gerenciamento.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +11,7 @@ import java.util.List;
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
 
     List<Projeto> findByUsuarioResponsavelId(Long usuarioId);
+
+    @Query("SELECT p FROM Projeto p JOIN FETCH p.cliente")
+    List<Projeto> findAllWithClientes();  // Busca todos os projetos com os clientes associados
 }
