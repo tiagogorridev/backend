cc3925a50855628ff01af023c9844393104735bf
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-14 13:33:37 -0300

    fix: ajustes nos nomes dos campos de cliente para garantir consistência

--- src/main/java/com/timetracker/sistema_gerenciamento/service/ClienteService.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/service/ClienteService.java
@@ -5,6 +5,8 @@ import com.timetracker.sistema_gerenciamento.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService {
 
@@ -12,22 +14,30 @@ public class ClienteService {
     private ClienteRepository clienteRepository;
 
     public Cliente adicionarCliente(Cliente cliente) {
-        if (cliente.getEmailCliente() == null || cliente.getEmailCliente().trim().isEmpty()) {
+        if (cliente.getEmail() == null || cliente.getEmail().trim().isEmpty()) {
             throw new IllegalArgumentException("O email do cliente não pode ser nulo ou vazio");
         }
 
-        if (cliente.getNomeCliente() == null || cliente.getNomeCliente().trim().isEmpty()) {
+        if (cliente.getNome() == null || cliente.getNome().trim().isEmpty()) {
             throw new IllegalArgumentException("O nome do cliente não pode ser nulo ou vazio");
         }
 
-        if (!cliente.getEmailCliente().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+        if (!cliente.getEmail().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
             throw new IllegalArgumentException("O email do cliente não é válido");
+        }
+
+        if (cliente.getStatus() == null || cliente.getStatus().isEmpty()) {
+            cliente.setStatus("ATIVO");
         }
 
         return clienteRepository.save(cliente);
     }
 
     public Cliente buscarPorEmail(String email) {
-        return clienteRepository.findByEmailCliente(email);
+        return clienteRepository.findByEmail(email);
+    }
+
+    public List<Cliente> listarTodosClientes() {
+        return clienteRepository.findAll();
     }
 }
