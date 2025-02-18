0be418fd0cf9f6d8991df9c5eb33ddb38b8e3377
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-18 11:17:56 -0300

    feat: Adicionada funcionalidade para criação de tarefas através do endpoint POST /api/tarefas no controlador TarefaController

---
+++ src/main/java/com/timetracker/sistema_gerenciamento/controller/TarefaController.java
@@ -0,0 +1,25 @@
+package com.timetracker.sistema_gerenciamento.controller;
+
+import com.timetracker.sistema_gerenciamento.model.Tarefa;
+import com.timetracker.sistema_gerenciamento.repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tarefas")
+public class TarefaController {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @PostMapping
+    public ResponseEntity<Tarefa> createTarefa(@RequestBody Tarefa tarefa) {
+        Tarefa novaTarefa = tarefaRepository.save(tarefa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaTarefa);
+    }
+}
