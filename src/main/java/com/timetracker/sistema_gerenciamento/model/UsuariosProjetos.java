ea3e33001da2f19521b44603dff4999b0c6c7bdb
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-19 20:03:28 -0300

    fix: criação da entidade UsuariosProjetos com os campos necessários para associar usuários e projetos, incluindo a data de associação

---
+++ src/main/java/com/timetracker/sistema_gerenciamento/model/UsuariosProjetos.java
@@ -0,0 +1,62 @@
+package com.timetracker.sistema_gerenciamento.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios_projetos")
+public class UsuariosProjetos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "id_projeto")
+    private Long idProjeto;
+
+    @Column(name = "data_associacao")
+    private LocalDateTime dataAssociacao;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(Long idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public LocalDateTime getDataAssociacao() {
+        return dataAssociacao;
+    }
+
+    public void setDataAssociacao(LocalDateTime dataAssociacao) {
+        this.dataAssociacao = dataAssociacao;
+    }
+}
