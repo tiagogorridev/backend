13811ac6b332c444ca4da58f71ba02d1e9a17e64
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-20 08:44:56 -0300

    fix: Adicionar query para buscar projeto com cliente associado na interface ProjetoRepository

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/ProjetoRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/ProjetoRepository.java
@@ -3,12 +3,16 @@ package com.timetracker.sistema_gerenciamento.repository;
 import com.timetracker.sistema_gerenciamento.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+    @Query("SELECT p FROM Projeto p LEFT JOIN FETCH p.cliente WHERE p.id = :id")
+    Optional<Projeto> findProjetoWithClienteById(@Param("id") Long id);
 
     List<Projeto> findByUsuarioResponsavelId(Long usuarioId);
 
