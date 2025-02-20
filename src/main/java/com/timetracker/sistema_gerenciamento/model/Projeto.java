2348af543f9654c9dfd51301c774308445c9d14d
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-20 08:45:35 -0300

    fix: Corrigir mapeamento ManyToOne entre Projeto e Cliente, com a referência JSON configurada corretamente

--- src/main/java/com/timetracker/sistema_gerenciamento/model/Projeto.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/model/Projeto.java
@@ -46,7 +46,6 @@ public class Projeto {
 
     @ManyToOne
     @JoinColumn(name = "id_cliente", referencedColumnName = "id", nullable = false)
-    @JsonBackReference
     private Cliente cliente;
 
     // Getters e Setters
