61a001e731183bd8c17bea7e9a4157d88ba79dcd
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-24 00:37:49 -0300

    feat: adicionar método para buscar lançamentos de horas por usuário ordenados por data e hora de início

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/LancamentoHorasRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/LancamentoHorasRepository.java
@@ -4,6 +4,9 @@ import com.timetracker.sistema_gerenciamento.model.LancamentoHoras;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LancamentoHorasRepository extends JpaRepository<LancamentoHoras, Long> {
+    List<LancamentoHoras> findByUsuarioIdOrderByDataDescHoraInicioDesc(Long usuarioId);
 }
