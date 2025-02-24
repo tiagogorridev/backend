ab0cb6fe80553389c72804aaf2729451e9d2055e
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-24 11:52:14 -0300

    feat: adicionado método findByIdAndProjetoId no TarefaRepository

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/TarefaRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/TarefaRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
     List<Tarefa> findByProjetoId(Long projetoId);
+    Optional<Tarefa> findByIdAndProjetoId(Long idtarefa, Long idprojeto);
 }
