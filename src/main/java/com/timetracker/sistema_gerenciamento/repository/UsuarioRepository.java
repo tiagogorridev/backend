51f5424d6c966a04726f69c2ada307be2245cb78
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-27 20:21:34 -0300

    feat: listagem de usuarios ativos

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/UsuarioRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/UsuarioRepository.java
@@ -4,7 +4,10 @@ import com.timetracker.sistema_gerenciamento.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
+    List<Usuario> findByAtivo(String ativo);
 }
