de52e72076efc51859176d22ceaa7beadcc94f96
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-27 14:53:28 -0300

    feat: adicionar filtro por exclusão lógica nas tarefas associadas a projetos

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/TarefaRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/TarefaRepository.java
@@ -2,6 +2,7 @@ package com.timetracker.sistema_gerenciamento.repository;
 
 import com.timetracker.sistema_gerenciamento.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    @Query("SELECT t FROM Tarefa t WHERE t.projeto.id = ?1 AND t.deletedAt IS NULL")
     List<Tarefa> findByProjetoId(Long projetoId);
-    Optional<Tarefa> findByIdAndProjetoId(Long idtarefa, Long idprojeto);
+
+    @Query("SELECT t FROM Tarefa t WHERE t.id = ?1 AND t.projeto.id = ?2 AND t.deletedAt IS NULL")
+    Optional<Tarefa> findByIdAndProjetoId(Long idTarefa, Long idProjeto);
 }
