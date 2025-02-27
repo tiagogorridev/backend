230647101872912769298c852f3856c4b4f2d1d0
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-27 14:53:16 -0300

    feat: adicionar filtro por exclusão lógica em consultas de usuários e projetos

--- src/main/java/com/timetracker/sistema_gerenciamento/repository/UsuariosProjetosRepository.java
+++ src/main/java/com/timetracker/sistema_gerenciamento/repository/UsuariosProjetosRepository.java
@@ -2,6 +2,7 @@ package com.timetracker.sistema_gerenciamento.repository;
 
 import com.timetracker.sistema_gerenciamento.model.UsuariosProjetos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,12 +10,15 @@ import java.util.Optional;
 
 @Repository
 public interface UsuariosProjetosRepository extends JpaRepository<UsuariosProjetos, Long> {
-    boolean existsByIdUsuarioAndIdProjeto(Long idUsuario, Long idProjeto);
-
-    List<UsuariosProjetos> findByIdProjeto(Long idProjeto);
-
-    List<UsuariosProjetos> findByIdUsuario(Long idUsuario);
-
+    @Query("SELECT up FROM UsuariosProjetos up WHERE up.idUsuario = ?1 AND up.idProjeto = ?2 AND up.deletedAt IS NULL")
     Optional<UsuariosProjetos> findByIdUsuarioAndIdProjeto(Long idUsuario, Long idProjeto);
 
+    @Query("SELECT up FROM UsuariosProjetos up WHERE up.idProjeto = ?1 AND up.deletedAt IS NULL")
+    List<UsuariosProjetos> findByIdProjeto(Long idProjeto);
+
+    @Query("SELECT up FROM UsuariosProjetos up WHERE up.idUsuario = ?1 AND up.deletedAt IS NULL")
+    List<UsuariosProjetos> findByIdUsuario(Long idUsuario);
+
+    @Query("SELECT CASE WHEN COUNT(up) > 0 THEN true ELSE false END FROM UsuariosProjetos up WHERE up.idUsuario = ?1 AND up.idProjeto = ?2 AND up.deletedAt IS NULL")
+    boolean existsByIdUsuarioAndIdProjeto(Long idUsuario, Long idProjeto);
 }
